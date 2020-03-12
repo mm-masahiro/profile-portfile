87519954a71f07f03b616285325ea0e89d2af744
Tetsu <tetete1118@gmail.com>
2020-03-13 01:12:26 +0900

    Revert "Introduce tailwind css"

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-// this file tells postcss to use tailwind and autoprefixer
-module.exports = {
-  plugins: [
-    require("tailwindcss")("./tailwind.config.js"),
-    require("autoprefixer")
-  ]
-}
