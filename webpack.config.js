adf631160d6993e97c23e68633f183348bb9ec08
mm-masahiro <hand5.mm9.mzh@gmail.com>
2020-03-15 17:47:07 +0900

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+// output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
+const path = require('path');
+
+module.exports = {
+    // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
+    mode: 'development',
+    // エントリーポイントの設定
+    // entry:'../js/index.js',
+      // 出力の設定
+    output: {
+         //  出力ファイルのディレクトリ名
+        path: `${__dirname}/Portfolio1`,
+         // 出力ファイル名
+        filename: "../dist/main.js"
+    }
+};
