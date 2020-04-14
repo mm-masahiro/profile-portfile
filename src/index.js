02d63ae884517ae260a19cabcd9429f5c11782f8
mm-masahiro <61375806+mm-masahiro@users.noreply.github.com>
2020-04-15 00:00:33 +0900

    Update src/index.js
    Co-Authored-By: Tetsu <tetete1118@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './css/styles.scss';
 import './css/styles2.scss';
 
 
-document.getElementById('show').addEventListener('click',function(){
+document.getElementById('show').addEventListener('click', function(){
     this.classList.toggle('open');
     document.getElementById('list').classList.toggle('open');
 });
