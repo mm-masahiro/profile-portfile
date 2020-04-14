4a7d6261fbf71a8b2af5ee6994183bc71ce73b2a
mm-masahiro <hand5.mm9.mzh@gmail.com>
2020-04-14 23:04:03 +0900

    画像の中央配置、ハンバーガーメニューの追加

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import { hoge } from "./sub";
 
 import './css/styles.scss';
 import './css/styles2.scss';
+
+var list = document.getElementById('list')
+
+document.getElementById('show').addEventListener('click',function(){
+    this.classList.toggle('open');
+    document.getElementById('list').classList.toggle('open');
+});
