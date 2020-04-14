4e1890ee9b4eded80057877107ce55c304d7fc8e
mm-masahiro <61375806+mm-masahiro@users.noreply.github.com>
2020-04-14 23:54:53 +0900

    Update src/index.js
    Co-Authored-By: Tetsu <tetete1118@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { hoge } from "./sub";
 import './css/styles.scss';
 import './css/styles2.scss';
 
-var list = document.getElementById('list')
 
 document.getElementById('show').addEventListener('click',function(){
     this.classList.toggle('open');
