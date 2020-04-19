91ca97ae03564a38a009a959b46314855087e409
tetsu5555 <tetete1118@gmail.com>
2020-04-20 00:11:28 +0900

    chore: rename

--- src/jsx/react.jsx
+++ src/jsx/react.jsx
@@ -2,30 +2,35 @@
 
 const e = React.createElement;
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
+const element = (
+  <h1 className="greeting">
+    Hello, world!
+  </h1>
+);
+
+class App extends React.Component {
+  constructor(  ) {
+    super(  );
     this.state = { liked: false };
   }
 
   render() {
     if (this.state.liked) {
-      return 'You liked this.';
+      return (
+        <h1>You liked this.</h1>
+      )
     }
 
-    // return e(
-    //   'button',
-    //   { onClick: () => this.setState({ liked: true }) },
-    //   'aaaa'
-    // );
-
     return (
+      <div>
+        { element }
         <button onClick={() => this.setState({ liked: true })}>
           Like
         </button>
+      </div>
     );
   }
 }
 
 const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(e(App), domContainer);
