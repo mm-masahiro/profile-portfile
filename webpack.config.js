714363c666e81781d95571e0c007f1bc1cbc2123
tetsu5555 <tetete1118@gmail.com>
2020-04-20 00:12:41 +0900

    chore: fix indent of webapack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,54 +3,54 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
-    mode: 'development',
-    // エントリーポイントの設定
-    entry: {
-        main: './src/index.js'
-    },
-     // 出力の設定
-    output: {
-         //  出力ファイルのディレクトリ名
-        path: `${__dirname}/public`,
-         // 出力ファイル名
-        filename: "main.js"
-    },
-    // webpack-dev-serverを立ち上げた時のドキュメントルートを設定
-    // ここではdistディレクトリのindex.htmlにアクセスするよう設定している
-    devServer: {
-        contentBase: path.join(`${__dirname}/public`), 
-        open: true,
-        hot: true,
-        port: 8080,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss/,
-                use: [
-                    "style-loader",
-                    {
-                        loader: "css-loader",
-                        options: {
-                            url: true,
-                            importLoaders: 2
-                        }
-                    },
-                    {
-                        loader: "sass-loader",
-                    }
-                ]
-            },
-            {
-                test: /\.(gif|png|jpeg|svg|jpg)$/,
-                loader: "url-loader"
+  // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
+  mode: 'development',
+  // エントリーポイントの設定
+  entry: {
+    main: './src/index.js'
+  },
+  // 出力の設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: `${__dirname}/public`,
+    // 出力ファイル名
+    filename: "main.js"
+  },
+  // webpack-dev-serverを立ち上げた時のドキュメントルートを設定
+  // ここではdistディレクトリのindex.htmlにアクセスするよう設定している
+  devServer: {
+    contentBase: path.join(`${__dirname}/public`),
+    open: true,
+    hot: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              url: true,
+              importLoaders: 2
             }
+          },
+          {
+            loader: "sass-loader",
+          }
         ]
-    },
-    plugins: [
-        new CopyPlugin ([
-            { from: `${__dirname}/public`, to: `${__dirname}/test` }
-        ])
+      },
+      {
+        test: /\.(gif|png|jpeg|svg|jpg)$/,
+        loader: "url-loader"
+      }
     ]
+  },
+  plugins: [
+    new CopyPlugin([
+      { from: `${__dirname}/public`, to: `${__dirname}/test` }
+    ])
+  ]
 };
