9cc1d3ebc7b423bfd685595644c4706d4ae329ce
tetsu5555 <tetete1118@gmail.com>
2020-04-21 01:28:50 +0900

    fix components

--- src/react/app.tsx
+++ src/react/app.tsx
@@ -1,36 +1,5 @@
 import * as React from 'react';
-import { render } from 'react-dom';
+import * as ReactDOM from 'react-dom';
+import { App } from "./components/App"
 
-import Button from "./Button";
-import Welcome from "./Welcome";
-
-const element = <h1 className="greeting">Hello, world!!!!!!!!!</h1>;
-
-class App extends React.Component<{
-  message: string
-}, {
-  count: number
-}> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    }
-  }
-
-  render() {
-    return (
-      <div onClick={this.increment}>{this.props.message} {this.state.count}</div>
-    )
-  }
-
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-}
-
-// export default App;
-
-render(<App message="this is props" />, document.getElementById('app'));
+ReactDOM.render(<App message="Hello again" />, document.getElementById('app'));
