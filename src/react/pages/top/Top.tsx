6e2128f0e38bbc872a5803ce3e559ae6a7b05c30
tetsu5555 <tetete1118@gmail.com>
2020-05-04 00:46:41 +0900

    add contentful

--- src/react/pages/top/Top.tsx
+++ src/react/pages/top/Top.tsx
@@ -1,6 +1,8 @@
-import * as React from 'react';
+import * as React  from 'react';
+import { useEffect }  from 'react';
 import { GlobalHeader } from '../../foundation/components/GlobalHeader';
 import { GlobalFooter } from '../../foundation/components/GlobalFooter';
+import ContentClient, * as contentClient from '../../foundation/utils/content_client'
 import './Top.css'
 
 type AppProps = {}
@@ -8,14 +10,30 @@ type AppProps = {}
 type AppState = {}
 
 export class Top extends React.Component<AppProps, AppState> {
+  state: any;
   constructor(props) {
     super(props);
+    this.state = {
+      items: []
+    }
+  }
+
+  async componentDidMount() {
+    
   }
 
   render() {
     return (
       <div className="top">
         <GlobalHeader />
+        {
+          this.state.items.map(item => (
+            <div>
+              {/* {JSON.stringify(item)} */}
+              {item.fields.title}
+            </div>
+          ))
+        }
         <div>
           <div>this is body</div>
           <div>this is body</div>
