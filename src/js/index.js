959d5abe4f4602cf2cb2e19a614ee89e9aa30618
tetsu5555 <tetete1118@gmail.com>
2020-05-04 00:47:05 +0900

    add

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,20 @@
 import "../css/styles.scss";
 import "../css/reset.css";
+
+const contentful = require("contentful");
+
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: "",
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: "",
+});
+
+// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
+
+client
+  .getEntries({ content_type: "post" })
+  .then((response) => console.log(response.items))
+  .catch(console.error);
+
+console.log("hogehgoehgoehgoeho");
