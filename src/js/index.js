c79ae7bc57f0b71e8616387c1b658c39b81fd8fa
tetsu5555 <tetete1118@gmail.com>
2020-05-04 16:48:49 +0900

    rounded cursor

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,23 @@
 import "../css/styles.scss";
 import "../css/reset.css";
+
+function setUpCursor() {
+  const cursor = document.querySelector(".cursor");
+  const cursorWidth = 20; // カーソルの大きさ
+  let mouseX = 0; // マウスのX座標
+  let mouseY = 0; // マウスのY座標
+
+  document.addEventListener("mousemove", (e) => {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    cursor.style.display = "block";
+    cursor.style.left = `${mouseX - cursorWidth / 2}px`;
+    cursor.style.top = `${mouseY - cursorWidth / 2}px`;
+  });
+}
+
+function init() {
+  setUpCursor();
+}
+
+init();
