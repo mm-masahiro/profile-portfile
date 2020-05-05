f1df1ccfff2d37961c574fe40c2f377502c95324
mm-masahiro <hand5.mm9.mzh@gmail.com>
2020-05-05 15:53:55 +0900

    スマホ対応時にスライダーを導入

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,21 @@
 import "../css/styles.scss";
 import "../css/reset.css";
+
+const $ = require("jquery");
+const Flipsnap = require("flipsnap");
+
+$(function () {
+  Flipsnap("#about_slide", {
+    distance: 280,
+  });
+});
+$(function () {
+  Flipsnap("#job_slide", {
+    distance: 280,
+  });
+});
+$(function () {
+  Flipsnap("#hobby_slide", {
+    distance: 280,
+  });
+});
