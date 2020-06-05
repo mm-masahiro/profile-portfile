377ac5393945be5290ef795565f5878faa044039
mm-masahiro <hand5.mm9.mzh@gmail.com>
2020-06-05 21:52:26 +0900

    test-deploy

--- webpack_configs/webpack.config.production.js
+++ webpack_configs/webpack.config.production.js
@@ -11,10 +11,10 @@ module.exports = merge(baseConfig, {
         from: path.join(__dirname, "../src/img"),
         to: path.join(__dirname, "../public/img"),
       },
-      {
-        from: path.join(__dirname, "../src/index.html"),
-        to: path.join(__dirname, "../public"),
-      },
+      // {
+      //   from: path.join(__dirname, "../src/index.html"),
+      //   to: path.join(__dirname, "../public"),
+      // },
       {
         from: path.join(__dirname, "../src/react.html"),
         to: path.join(__dirname, "../public"),
